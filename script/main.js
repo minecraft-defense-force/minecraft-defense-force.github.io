4e1b8ecb3edbbdc048604602b7cce62846353a96
anatawa12 <anatawa12@users.noreply.github.com>
2022-02-21 10:49:01 +0000

    deploy: dacb2cc2c368cb90bb2841c8b4d2326c8291641b

--- script/main.js
+++ script/main.js
@@ -14,7 +14,7 @@ $(window).on('load', function () {
 		$("#browser").toggleClass("show");
 	}
 
-	$('.loading').delay(1200).fadeOut(300);
+	$('.loading').delay(500).fadeOut(300);
 
 	!function webcrow_referer_warn() {
 		if (document.referrer === "") return;
@@ -55,6 +55,8 @@ $(window).on('load', function () {
 			$("#browser").before(referer_warn)
 		}
 	}()
+
+	!function dead_warnings() { if (document.location.pathname.endsWith("/death.html")) return; /** @return HTMLElement */ function findHeader() { return document.getElementById("header-inner") || document.getElementsByClassName("layout-header")[0]; } var header = findHeader(); if (header == null) { console.log("header not found"); return; } console.log("header", header); var height = header.offsetHeight; console.log("height", height); header.style.height = (height * 2) + "px"; var noticeDiv = document.createElement("div"); noticeDiv.style.position = "absolute"; noticeDiv.style.top = height + "px"; noticeDiv.style.height = height + "px"; noticeDiv.style.left = "0"; noticeDiv.style.width = "100%"; noticeDiv.style.backgroundColor = "rgb(255, 47, 47)"; var noticeA = document.createElement("a"); noticeA.append("マインクラフト防衛部連邦の終了について"); noticeA.href = "/news/death.html"; noticeA.style.position = "relative"; noticeA.style.left = "50%"; noticeA.style.transform = "translateX(-50%)"; noticeA.style.display = "inline-block"; noticeA.style.width = "auto"; noticeA.style.textAlign = "center"; noticeA.style.lineHeight = "48px"; noticeA.style.color = "rgb(231, 248, 255)"; noticeA.style.textDecoration = "none"; noticeDiv.append(noticeA); header.append(noticeDiv); /* navigation */ var nav = document.getElementsByClassName("layout-nav")[0]; if (nav == null) { console.log("nav not found"); return; } nav.style.top = (height * 2) + "px"; }() 
 });
 
 
